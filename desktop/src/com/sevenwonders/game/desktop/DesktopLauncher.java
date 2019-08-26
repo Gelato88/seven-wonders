a117a8a61c3c2ac34864e5bbc82239ab326e1d54
Scott Lin <scottj.lin@gmail.com>
2019-08-26 09:52:39 -0700

    Adding gradle files

---
+++ desktop/src/com/sevenwonders/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.sevenwonders.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.sevenwonders.game.SevenWonders;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new SevenWonders(), config);
+	}
+}
