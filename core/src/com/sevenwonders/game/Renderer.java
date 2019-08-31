f64d7f26a70475faea58e63a1107327b8268fdc3
Scott Lin <scottj.lin@gmail.com>
2019-08-31 11:18:39 -0700

    added freetype dependencies; starting displaying resources

--- core/src/com/sevenwonders/game/Renderer.java
+++ core/src/com/sevenwonders/game/Renderer.java
@@ -1,12 +1,14 @@
 package com.sevenwonders.game;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.GlyphLayout;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
 import com.sevenwonders.game.Cards.Card;
 
 import java.util.ArrayList;
@@ -16,7 +18,10 @@ public class Renderer {
     private SpriteBatch batch;
     private SevenWonders game;
     private GlyphLayout layout;
-    private BitmapFont font;
+    private BitmapFont whiteFont;
+    private BitmapFont brownFont;
+    private BitmapFont grayFont;
+    private BitmapFont yellowFont;
 
     Texture img;
     Texture topBarBackground;
@@ -25,9 +30,29 @@ public class Renderer {
     Renderer(SpriteBatch batch, SevenWonders game) {
         this.batch = batch;
         this.game = game;
+        layout = new GlyphLayout();
         topBarBackground = new Texture(Gdx.files.internal("textures/TopBarBackground.png"));
         img2 = new Sprite(topBarBackground);
         img2.setPosition(0, 880);
+        loadFonts();
+    }
+
+    private void loadFonts() {
+        FreeTypeFontGenerator generator = new FreeTypeFontGenerator(Gdx.files.internal("fonts/LuckiestGuy-Regular.ttf"));
+        FreeTypeFontGenerator.FreeTypeFontParameter parameter = new FreeTypeFontGenerator.FreeTypeFontParameter();
+        parameter.size = 48;
+        parameter.color = new Color(184/255f, 77/255f, 28/255f, 1f);
+        parameter.shadowColor = Color.BLACK;
+        parameter.shadowOffsetX = 3;
+        parameter.shadowOffsetY = 1;
+        brownFont = generator.generateFont(parameter);
+        parameter.color = new Color(160/255f, 180/255f, 184/255f, 1f);
+        grayFont = generator.generateFont(parameter);
+        parameter.color = new Color(224/255f, 215/255f, 36/255f, 1f);
+        yellowFont = generator.generateFont(parameter);
+        parameter.color = Color.WHITE;
+        whiteFont = generator.generateFont(parameter);
+        generator.dispose();
     }
 
     private void drawCurrentPlayerHand() {
@@ -44,6 +69,30 @@ public class Renderer {
             xPos += xIncrement;
         }
         batch.end();
+    }
+
+    private void drawCurrentPlayerResources() {
+        Player p = game.currentPlayer;
+        float increment = 60f;
+
+        batch.begin();
+        writeResourceLine("" + p.coins, whiteFont, yellowFont, 80f, 1000f);
+        writeResourceLine("" + p.lumber, whiteFont, brownFont, 80f, 1000f - increment * 1);
+        writeResourceLine("" + p.ore, whiteFont, brownFont, 80f, 1000f - increment * 2);
+        writeResourceLine("" + p.clay, whiteFont, brownFont, 80f, 1000f - increment * 3);
+        writeResourceLine("" + p.stone, whiteFont, brownFont, 80f, 1000f - increment * 4);
+        writeResourceLine("" + p.textile, whiteFont, grayFont, 80f, 1000f - increment * 5);
+        writeResourceLine("" + p.glass, whiteFont, grayFont, 80f, 1000f - increment * 6);
+        writeResourceLine("" + p.papyrus, whiteFont, grayFont, 80f, 1000f - increment * 7);
+        batch.end();
+    }
+
+    private void writeResourceLine(String text, BitmapFont whiteFont, BitmapFont font, float x, float y) {
+        layout.setText(whiteFont, " - ");
+        float incrememnt = layout.width;
+        whiteFont.draw(batch, layout, x, y);
+        layout.setText(font, text);
+        font.draw(batch, layout, x + incrememnt, y);
     }
 
     private void drawSelectedCard() {
@@ -65,6 +114,10 @@ public class Renderer {
         batch.end();
         drawCurrentPlayerHand();
         drawSelectedCard();
+        drawCurrentPlayerResources();
+
+
+
     }
 
 }
